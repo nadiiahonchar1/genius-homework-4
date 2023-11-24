e6ea277b6df71f293d902d4088638deb3925c621
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2023-11-24 11:38:13 +0200

    fixed mobile script

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,10 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
+const linkMenu = document.querySelectorAll('.link-burger-js');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+linkMenu.forEach(i => i.addEventListener('click', toggleMenu));
